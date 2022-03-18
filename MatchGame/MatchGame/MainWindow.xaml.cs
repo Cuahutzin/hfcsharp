1a2947d6cd97e94ca1bc1ffc38e6771d4e0b9213
Eduardo Chavez <eduardoc.chavezh@gmail.com>
2022-03-18 16:25:07 -0600

    20 min work , add emojis

--- MatchGame/MatchGame/MainWindow.xaml.cs
+++ MatchGame/MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,29 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+            SetUpGame();
+        } 
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+               "😘", "😘",
+               "😊", "😊",
+               "😂", "😂",
+               "❤", "❤",
+               "👌", "👌",
+               "😒", "😒",
+               "😍", "😍",
+               "🤣", "🤣"
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
         }
     }
 }
